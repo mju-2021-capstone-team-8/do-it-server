9033b797d999369d8168c01cef46e67793a2e76d
13circle <13circle97@gmail.com>
2021-04-06 22:30:54 +0900

    Add Tempo-Rendering API

--- src/api/game/game.ctrl.ts
+++ src/api/game/game.ctrl.ts
@@ -2,7 +2,13 @@ import { Request, Response } from "express";
 
 export const info = (req: Request, res: Response) => {
   res.status(200).json({
-    msg: "Song Info Test",
+    msg: "API Test - Song Info",
+  });
+};
+
+export const renderTempo = (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "API Test - Render Tempo from music",
   });
 };
 
--- src/api/game/index.ts
+++ src/api/game/index.ts
@@ -5,6 +5,7 @@ import * as gameCtrl from "./game.ctrl";
 const game = Router();
 
 game.get("/info", gameCtrl.info);
+game.post("/render-tempo", gameCtrl.renderTempo);
 
 export default game;
 
