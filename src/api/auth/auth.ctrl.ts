c05b6e97041d56b326ef8dcf30bb260f2323afb3
13circle <13circle97@gmail.com>
2021-04-09 23:15:26 +0900

    Set Auth APIs frame structure

--- src/api/auth/auth.ctrl.ts
+++ src/api/auth/auth.ctrl.ts
@@ -1,8 +1,32 @@
 import { Request, Response } from "express";
 
+export const info = (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "User Info Test",
+  });
+};
+
+export const register = (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "User Registration Test",
+  });
+};
+
 export const login = (req: Request, res: Response) => {
   res.status(200).json({
     msg: "Login Test",
+  });
+};
+
+export const edit = (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "Edit Test",
+  });
+};
+
+export const unregister = (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "User Unregistration Test",
   });
 };
 
--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -4,7 +4,11 @@ import * as authCtrl from "./auth.ctrl";
 
 const auth = Router();
 
+auth.get("/info", authCtrl.info);
+auth.post("/register", authCtrl.register);
 auth.post("/login", authCtrl.login);
+auth.patch("/edit", authCtrl.edit);
+auth.delete("/unregister", authCtrl.unregister);
 
 export default auth;
 
